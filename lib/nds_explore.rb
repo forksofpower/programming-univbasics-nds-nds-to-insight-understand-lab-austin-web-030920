dd1bcfac90d7c465247f478365338c4584c51495
Patrick Jones <patrickjones.pmj@gmail.com>
2020-03-04 14:41:48 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,4 +8,10 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  # get Spielberg
+  director = directors_database[0]
+  # create list of movie titles
+  movies = director[:movies]
+  # print list of director's movie titles
+  puts movies.map { |movie| movie[:title]  }
 end
